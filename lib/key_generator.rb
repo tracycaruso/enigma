7f0ccf978242e7663a48a645d8cfa768a6cd58ec
ycartc <tracymcaruso@gmail.com>
2015-03-09 15:53:10 -0600

    Refactor KeyGenerator

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,5 +1,5 @@
 class KeyGenerator
   def generate_key
-    rand(0..9.0).to_s.slice(2..6)
+    rand(0..9.0).to_s.slice(3..7).to_i
   end
 end
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -8,12 +8,17 @@ class KeyGeneratorTest < Minitest::Test
 
   def test_genertated_key_is_five_characters_long
     key = KeyGenerator.new
-    assert_equal 5, key.generate_key.length
+    assert_equal 5, key.generate_key.to_s.length
   end
 
   def test_it_generates_a_random_key
     key = KeyGenerator.new
     keys = 10.times.map { key.generate_key }
     assert_equal keys.length, keys.uniq.length
+  end
+
+  def test_that_key_is_a_fixnum
+    key = KeyGenerator.new
+    assert_instance_of Fixnum, key.generate_key
   end
 end
