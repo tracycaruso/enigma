4e1902cebdf3a779247b9ca31dbe051e65358e8b
ycartc <tracymcaruso@gmail.com>
2015-03-09 18:54:59 -0600

    Refactor encrypt, decrypt, and cracker runner files

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -4,7 +4,7 @@ require_relative 'rotator'
 require_relative 'enigma'
 
 class Decrypt
-  attr_reader :target_filename, :key, :date, :decrypted_message
+  attr_reader :target_filename, :key, :date, :enigma, :decrypted_message
   def initialize
     message_filename   = ARGV[0]
     @target_filename   = ARGV[1]
@@ -12,15 +12,23 @@ class Decrypt
     @date              = ARGV[3]
     message            = File.open(message_filename, "r"){ |file| file.read }.chomp.to_s
     rotator            = Rotator.new(key.to_i, date.to_i)
-    enigma             = Enigma.new(message, rotator)
+    @enigma            = Enigma.new(message, rotator)
+  end
+
+  def decrypt_message
     @decrypted_message = enigma.decrypt
   end
 
   def write_file
     File.open(target_filename, "w"){ |file| file.puts decrypted_message}
+  end
+
+  def success_message
     puts "Created '#{target_filename}' with the key #{key} and date #{date}"
   end
 end
 
 decrypt = Decrypt.new
+decrypt.decrypt_message
 decrypt.write_file
+decrypt.success_message
--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,11 +1,10 @@
-#!/usr/bin/env ruby
 require_relative 'key_generator'
 require_relative 'date_generator'
 require_relative 'rotator'
 require_relative 'enigma'
 
 class Encrypt
-  attr_reader :message_filename, :target_filename, :key, :date, :encrypted_message
+  attr_reader :message_filename, :target_filename, :key, :date, :enigma, :encrypted_message
   def initialize
     @message_filename  = ARGV[0]
     @target_filename   = ARGV[1]
@@ -13,14 +12,23 @@ class Encrypt
     @key               = KeyGenerator.new.generate_key
     @date              = DateGenerator.new.generate_date
     rotator            = Rotator.new(key.to_i, date.to_i)
-    enigma             = Enigma.new(message, rotator)
+    @enigma            = Enigma.new(message, rotator)
+  end
+
+  def encrypt_message
     @encrypted_message = enigma.encrypt
   end
 
   def write_file
     File.open(target_filename, "w"){ |file| file.puts encrypted_message}
+  end
+
+  def success_message
     puts "Created '#{message_filename}' with the key #{key} and date #{date}"
   end
 end
-encrypt = Encrypt.new
-encrypt.write_file
+
+# encrypt = Encrypt.new
+# encrypt.encrypt_message
+# encrypt.write_file
+# encrypt.success_message
